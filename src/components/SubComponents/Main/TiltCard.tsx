dd3f40fe7dd5a5134deb79733e177218bb7ab23b
Furkan Ercan <furkanduhannuman@gmail.com>
2024-04-14 18:39:13 +0300

    fixed tiltcard padding -2

--- src/components/SubComponents/Main/TiltCard.tsx
+++ src/components/SubComponents/Main/TiltCard.tsx
@@ -46,7 +46,7 @@ const TiltCard = ({item,i}:Props) => {
     }
 
     return (
-        <MotionDiv whileInView={{opacity:[0,1]}} viewport={{ once: true, amount: 0 }} transition={{duration:1,delay:0.3*i}} style={{rotateX,rotateY}} className='hover:shadow shadow-white hover:bg-[#0a0a0a] lg:m-10 border text-center p-2 lg:p-4 rounded-xl flex flex-col items-center justify-center w-[40vw] h-auto lg:w-3/4 gap-4' onMouseMove={handleMouseMove} onMouseOut={handleMouseOut}>
+        <MotionDiv whileInView={{opacity:[0,1]}} viewport={{ once: true, amount: 0 }} transition={{duration:1,delay:0.3*i}} style={{rotateX,rotateY}} className='hover:shadow shadow-white hover:bg-[#0a0a0a] lg:m-10 border text-center p-2 lg:p-4 rounded-xl flex flex-col items-center justify-center w-[40vw] h-[25vh] lg:h-auto lg:w-3/4 gap-4' onMouseMove={handleMouseMove} onMouseOut={handleMouseOut}>
             <div className='w-[150] h-[150px] lg:w-full lg:h-[400px] object-contain overflow-hidden'><img src={item.photo} alt="" className='object-cover' /></div>
             <span className='text-xl'>{item.name}</span>
       </MotionDiv>
