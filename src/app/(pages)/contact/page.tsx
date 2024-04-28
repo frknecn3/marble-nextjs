8fa9167955015008a243e7d2867d51d1192e7145
Furkan Ercan <furkanduhannuman@gmail.com>
2024-04-28 21:14:34 +0300

    fix opt error 1

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,38 +1,42 @@
-import React from 'react'
-import { CgMail } from 'react-icons/cg'
-import { CiMail } from 'react-icons/ci'
-import { FaWhatsapp } from 'react-icons/fa'
-import { FiMail } from 'react-icons/fi'
+import React, { FC } from 'react';
+import { CgMail } from 'react-icons/cg';
+import { CiMail } from 'react-icons/ci';
+import { FaWhatsapp } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
 
-const Contact = ({long=true}:{long:boolean}) => {
-  return (
-    <div className={`flex flex-col lg:block items-center justify-center ${long?'min-h-[100vh]':'min-h-0'}`}>
-        <div className='mt-20 mb-[10vh]  lg:text-center'><h1 className='text-5xl font-bold'>Bizimle İletişime Geçin</h1></div>
-
-        <div className='flex lg:flex-row flex-col gap-40 justify-between mt-10 mx-[10vw] lg:mx-[20vw]'>
-            <div className='text-2xl flex flex-col gap-4'>
-                <p>E-posta: info@portamarble.com</p>
-                <p>Whatsapp: +90 000 000 00 00</p>
-                <p>Telefon: +90 212 000 00 00</p>
-                <p>E-posta: lorem@ipsum.com</p>
-            </div>
-            
-            <div className='flex flex-col justify-between gap-4'>
-                <a target='_blank' href="https://wa.me/00000000011"
-                className='text-3xl bg-green-500 p-4 rounded-xl flex gap-2 items-center'> 
-                <FaWhatsapp className='text-5xl' />
-                WHATSAPP
-                </a>
-
-                <a className=' text-white border border-white bg-neutral-700 items-center gap-2 text-3xl flex p-4 rounded-xl' href='mailto:lorem@ipsum.com'>
-                <CiMail className='text-5xl text-white '/>
-                E-POSTA
-                </a>
-            </div>
-        </div>
-        
-    </div>
-  )
+interface ContactProps {
+  long?: boolean;
 }
 
-export default Contact
+const Contact: FC<ContactProps> = ({ long = true }) => {
+  return (
+    <div className={`flex flex-col lg:block items-center justify-center ${long ? 'min-h-[100vh]' : 'min-h-0'}`}>
+      <div className='mt-20 mb-[10vh]  lg:text-center'><h1 className='text-5xl font-bold'>Bizimle İletişime Geçin</h1></div>
+
+      <div className='flex lg:flex-row flex-col gap-40 justify-between mt-10 mx-[10vw] lg:mx-[20vw]'>
+        <div className='text-2xl flex flex-col gap-4'>
+          <p>E-posta: info@portamarble.com</p>
+          <p>Whatsapp: +90 000 000 00 00</p>
+          <p>Telefon: +90 212 000 00 00</p>
+          <p>E-posta: lorem@ipsum.com</p>
+        </div>
+
+        <div className='flex flex-col justify-between gap-4'>
+          <a target='_blank' href="https://wa.me/00000000011"
+            className='text-3xl bg-green-500 p-4 rounded-xl flex gap-2 items-center'>
+            <FaWhatsapp className='text-5xl' />
+            WHATSAPP
+          </a>
+
+          <a className=' text-white border border-white bg-neutral-700 items-center gap-2 text-3xl flex p-4 rounded-xl' href='mailto:lorem@ipsum.com'>
+            <CiMail className='text-5xl text-white' />
+            E-POSTA
+          </a>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Contact;
